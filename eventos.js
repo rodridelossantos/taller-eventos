68273046edd79fb58d504a9ab79220fb5db5480b
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2023-08-12 16:54:15 -0300

    Arreglar burbujeo de eventos

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.getElementsByTagName("div")[0];
+const btn = document.getElementsByTagName("button")[0];
 
 div.addEventListener("click", () => {
   alert("Hola! soy el div");
+});
+
+btn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  alert("Hola!");
 });
